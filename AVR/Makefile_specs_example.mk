2dd88d6804dd772148b9c642a81b46aee9f87c09
Maurik Holtrop <maurik@physics.unh.edu>
2016-12-11 12:22:51 -0500

    modernize it.

--- AVR/Makefile_specs_example.mk
+++ AVR/Makefile_specs_example.mk
@@ -11,13 +11,13 @@
 # Type of board, for the pin layout library.
 # You find the possible board definitions in ../include/variants
 #
-BOARD_TYPE=atmega168
+BOARD_TYPE=atmega328p
 
 # The type of the chip as the avr-gcc compiler expects it.
 # You can find the types of chips supported with "avr-gcc -dumpspecs | grep mmcu="
 #
 # Examples: atmega168p  atmega328p  atmega168 
-MCU=atmega168
+MCU=atmega328p
 
 # Frequency of CPU.
 # Without a crystal, this is 1 MHz standard, or 8 MHz with a fuse setting.
@@ -31,7 +31,9 @@ F_CPU=20000000
 # This is useful if you are working with different chips.
 # Make sure that your program is linked with the correct library for your chip.
 #
-SYSTEM_TYPE=atmega168_20MHz
+SYSTEM_TYPE=$(MCU)_$(F_CPU)
+# Alternate, specify by hand:
+#SYSTEM_TYPE=atmega168_20MHz
 
 # id to use with programmer
 # default: PROGRAMMER_MCU=$(MCU)
@@ -39,20 +41,23 @@ SYSTEM_TYPE=atmega168_20MHz
 # accept the same MCU name as avr-gcc (for example
 # for ATmega8s, avr-gcc expects 'atmega8' and 
 # avrdude requires 'm8')
+# Newer avrdude does better with that!
+#
+PROGRAMMER_MCU=$(MCU)
 
 #
-# For an Arduino Mega, programming through USB.
-#
-ifeq ($(MCU),atmega328)
-PROGRAMMER_MCU=m328
-endif
-
-ifeq ($(MCU),atmega168)
-PROGRAMMER_MCU=m168
-endif
-
-# 
+# Name of the programmer to use. This is defined in the
+# file: /usr/local/etc/avrdude.conf
+# Or in  ~/.avrduderc
+# For the Raspberry pi, we defined this in the latter file
+# to use linuxgpio with specific pins.
 AVRDUDE_PROGRAMMERID=RaspberryPi
+
+#
+# If you use the Avrisp mkII programmer
+#
+#AVRDUDE_PROGRAMMERID=avrisp2
+#AVRDUDE_PORT=usb
 
 
 # port--serial or parallel port to which your 
@@ -62,6 +67,5 @@ AVRDUDE_PROGRAMMERID=RaspberryPi
 #AVRDUDE_PORT=/dev/tty.usbserial-A6008hex
 #AVRDUDE_EXTRA_FLAGS= -F -b57600 -V
 #
-AVRDUDE_PORT=usb
 
 
