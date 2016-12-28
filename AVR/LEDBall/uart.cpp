a1747d7985477a61b13b445621b2b5964527d882
Maurik Holtrop <maurik@physics.unh.edu>
2016-12-27 21:30:01 -0500

    New serial code added from UART dir.

---
+++ AVR/LEDBall/uart.cpp
@@ -0,0 +1,243 @@
+#include <stdint.h>
+#include <stdio.h>
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#include "uart.hpp"
+
+// File handle for the uart
+FILE __uart_io;
+
+#if UART_USE_BUFFERS == 1
+// Buffers for the buffered uart
+char __tx_uart_buf[TX_UART_BUF_SIZE];
+char __rx_uart_buf[RX_UART_BUF_SIZE];
+
+// Buffer pointers, to make a circular buffer.
+__tx_uart_buf_index_t __tx_uart_buf_head;
+__tx_uart_buf_index_t __tx_uart_buf_tail;
+__tx_uart_buf_index_t __rx_uart_buf_head;
+__tx_uart_buf_index_t __rx_uart_buf_tail;
+
+bool __rx_uart_receive_complete;
+#endif
+
+// Global flag, telling any code that a complete string was received.
+
+#if UART_ALLOW_BAUD == 1
+void uart_init(unsigned char mode,uint32_t baud){
+  // Setup the UART and the two FILE objects for input and output.
+  // Set baudrate explicitly from the program. Same calculation as setbaud.h. Extra 320 bytes if code.
+  uint16_t ubbr_value=(((F_CPU) + 8UL * (baud)) / (16UL * (baud)) -1UL);
+  
+  if(100UL*(F_CPU) >  (16 * ((ubbr_value) + 1)) * (100 * (baud) + (baud) * (BAUD_TOL)) ||
+     100 * (F_CPU) <  (16 * ((ubbr_value) + 1)) * (100 * (baud) - (baud) * (BAUD_TOL)) ){
+    // Outside of tolerance, use USE2X formula
+    ubbr_value  = (((F_CPU) + 4UL * (baud)) / (8UL * (baud)) -1UL);
+    UCSR0A |= _BV(U2X0);
+  }else{
+    UCSR0A &= ~(_BV(U2X0));
+  }
+  
+  UBRR0H = (ubbr_value >> 8);
+  UBRR0L = (ubbr_value & 0xFF);
+ 
+#else // UART_ALLOW_BAUD
+
+#warning Baudrate is fixed at BAUD.
+void uart_init(unsigned char mode, uint32_t /* baud is discarded */) {
+  // Setup the UART and the two FILE objects for input and output.
+  // This part uses the fixed BAUD set in the header. "setbaud.h" does the calculation.
+  
+#include <util/setbaud.h>
+  
+  UBRR0H = UBRRH_VALUE;
+  UBRR0L = UBRRL_VALUE;
+  
+#if USE_2X
+  UCSR0A |= _BV(U2X0);
+#else
+  UCSR0A &= ~(_BV(U2X0));
+#endif
+  
+#endif // UART_ALLOW_BAUD
+
+// The second part of the init sets up the UART and links either the
+// blocking (mode=0) or the buffered (mode=1) or a combo with the FILE handle.
+//
+  UCSR0C  = _BV(UCSZ01) | _BV(UCSZ00); // 8-bit data, no parity, 1 stop bit = 8N1 = 0x06
+  UCSR0B  = _BV(RXEN0) | _BV(TXEN0);   // Enable RX and TX
+
+#if UART_USE_BUFFERS == 1
+  if( (mode&0x01) == 0){ // unbuffered transmission, which is better for debugging.
+    __uart_io.put = uart_putchar;
+  }else{                       // Buffered output, better for 
+    __uart_io.put = uart_putchar_buffered;
+  }
+    
+  if( (mode&0x02) == 0){
+    __uart_io.get = uart_getchar;
+    __rx_uart_receive_complete=true;  // Not buffering, so always complete.
+  }else{
+    __uart_io.get = uart_getchar_buffered;
+    UCSR0B |= _BV(RXCIE0);                // Receive interrupt enabled.
+    __rx_uart_receive_complete=false;  // No, no <CR> received yet.
+  }
+  __tx_uart_buf_head = 0;
+  __tx_uart_buf_tail = 0;
+  __rx_uart_buf_head = 0;
+  __rx_uart_buf_tail = 0;
+
+#else
+  __uart_io.put = uart_putchar;
+  __uart_io.get = uart_getchar;
+#endif
+
+  __uart_io.flags=  _FDEV_SETUP_RW;
+  
+  stdout = &__uart_io;
+  stdin  = &__uart_io;
+  
+}
+
+
+void uart_tx_buffer_flush(void){
+  // Block until the buffer is empty.
+  // Does nothing for unbuffered output.
+#if UART_USE_BUFFERS == 1
+  while(UCSR0B & _BV(UDRIE0));          // As long as the interrupt is enabled, we have stuff in the buffer.
+  loop_until_bit_is_set(UCSR0A, UDRE0); // And wait for the shift register buffer to be empty.
+  loop_until_bit_is_set(UCSR0A, TXC0 ); // And the last bits are shifted out.
+#endif
+}
+
+int uart_putchar(char c, FILE *stream) {
+  // This is a blocking version, it will wait until the UART is ready
+  // and then output the character. This means it is synchronous.
+  loop_until_bit_is_set(UCSR0A, UDRE0);
+  UDR0 = c;
+  return 0;
+}
+
+#if UART_USE_BUFFERS == 1
+int uart_putchar_buffered(char c, FILE *stream){
+  // Just put stuff in the circular buffer.
+  // If it is full, well, too bad, overwrite and get garbage out :-)
+  // This is asynchronous, we have the interrupt routine empty the buffer for us.
+  // The program keeps going, but can be interrupted to full the UART again.
+  if(__tx_uart_buf_head == __tx_uart_buf_tail && bit_is_set(UCSR0A,UDRE0) ){
+    // The buffer was empty, AND the UART TX (UCSR0A _BV(UDRE0) is empty.)
+    // UCSR0A = USART Control and status register A
+    // UDRE0  = USART Data Register Empty.
+    UDR0 = c;            // Send the char out right away.
+    UCSR0A|=  _BV(TXC0); // Clear the transmit complete bit.
+    return 0;            // Yup, it was send out. 0 = no error in fwrite.
+  }
+
+  // Here we check if adding this char will cause a buffer full.
+  // This must be done *before* actually incrementing the head,
+  // since we could be interrupted in the middle by the USART_UDRE_vect routine.
+  __tx_uart_buf_index_t next_buffer_loc = (__tx_uart_buf_head + 1) % TX_UART_BUF_SIZE;
+
+  
+  if(next_buffer_loc == __tx_uart_buf_tail ){  // Only one last spot left in the buffer!
+#if UART_BLOCK_ON_OVERFLOW == 1
+    while(next_buffer_loc == __tx_uart_buf_tail){;} // Block until there is space cleared.
+#else  
+    
+#warning Not blocking on overflow.
+    // We are not allowed to block. We can either overwrite or we can discard.
+    // Discarding seems safer. Mark the occurrence of the overflow by replacing the
+    // last bit in the buffer by an @.
+    if( __tx_uart_buf_head != 0){
+      __tx_uart_buf[__tx_uart_buf_head-1] = '@'; // Last char is overwritten.
+    }else{
+      __tx_uart_buf[TX_UART_BUF_SIZE-1] = '@';
+    }
+    // HEAD is NOT increased (then buffer would look empty! So return.
+    return 0;
+#endif
+  }
+
+  __tx_uart_buf[__tx_uart_buf_head] = c;
+  __tx_uart_buf_head = next_buffer_loc;
+
+  
+  UCSR0B |= _BV(UDRIE0); // Set USART Data Register Empty Interrupt Enable,
+                         // so when the current transmit is complete, the interupt will send this one.
+  return 0;
+}
+
+#endif // UART_USE_BUFFERS
+  
+int uart_getchar(FILE *stream) {
+// This is a blocking version. It will sit and wait until the UART has
+// a character for us.
+  loop_until_bit_is_set(UCSR0A, RXC0);
+  char c= UDR0;
+#if UART_ECHO == 1
+   loop_until_bit_is_set(UCSR0A, UDRE0);
+  UDR0 = c; // Echo it back out right away.
+#endif
+  if( c == 13 /* ^m */ || c == '\r') c='\n'; // End of line
+  return c;
+}
+
+#if UART_USE_BUFFERS == 1
+int uart_getchar_buffered(FILE *stream) {
+// This is the non blocking version. It looks for data from the rx buffer.
+// if the rx buffer is empty, unset __rx_uart_receive_complete and return '\n'
+// Note: We return '\n' and not 0 because fgets expects an \n at the end of the line.
+//
+  if(__rx_uart_buf_tail != __rx_uart_buf_head){ // We did not yet catch up with the head.
+    char c=__rx_uart_buf[ __rx_uart_buf_tail ];
+    __rx_uart_buf_tail = (__rx_uart_buf_tail + 1) % RX_UART_BUF_SIZE; //next_buffer_loc;
+    return c;
+  }else{ // We caught up with the tail. The buffer is empty, so be ready for next receive.
+    __rx_uart_receive_complete=false;
+    return '\0'; // Terminate the string.
+  }
+  return -1;
+}
+  
+/////////////////////////////////////////////////
+// INTERRUPT ROUTINES                         ///
+/////////////////////////////////////////////////
+
+ISR(USART_UDRE_vect){
+  // This is called when the UART is EMPTY, so we can send the next char.
+  UDR0 = __tx_uart_buf[__tx_uart_buf_tail]; // Send out the next char.
+  __tx_uart_buf_tail = (__tx_uart_buf_tail + 1) % TX_UART_BUF_SIZE; // increment the tail
+  // UCSR0A |= _BV(TXC0); // Clear the transmit complete bit.
+  
+  if(__tx_uart_buf_tail == __tx_uart_buf_head){ // We caught up, no next one to send.
+    UCSR0B &= ~_BV(UDRIE0);
+  }
+}
+
+ISR(USART_RX_vect){
+  // Receive something interrupt. The UART has an input ready for us.
+  char c = UDR0; // Read it right away.
+  __rx_uart_buf_index_t rx_i = (__rx_uart_buf_head + 1) % RX_UART_BUF_SIZE;
+  if(rx_i != __rx_uart_buf_tail){                     // Buffer still has space.
+    if( c == 13 /* ^m */ || c == '\n' || c == '\r'){  // End of line
+      __rx_uart_receive_complete=true; // Tell the code that we have something complete.
+      __rx_uart_buf[__rx_uart_buf_head] = 0; // Terminate string with 0, eat the c.
+    }else{
+      __rx_uart_buf[__rx_uart_buf_head] = c;
+      __rx_uart_buf_head = rx_i;
+    }
+#if UART_ECHO == 1
+    loop_until_bit_is_set(UCSR0A, UDRE0);
+    UDR0 = c; // Echo it back out right away.
+#endif
+  }else{ // Buffer is full.
+    // Now what? - we *could* block, or we can throw the input away.
+    // If we throw it away, do we throw the entire buffer?
+    __rx_uart_receive_complete=true; // Pretend you got a return, so process the buffer.
+  }
+}
+  
+#endif // UART_USE_BUFFERS
+
---
+++ AVR/LEDBall/uart.hpp
@@ -0,0 +1,114 @@
+/**
+ *  UART Code for use with avr-lib printf
+ *
+ * This is code for the AVR UART (USART actually) on the ATmega328/168 type chips.
+ * It will probably work on other chips as well, but you may need to change the registers used, i.e.
+ * UCSR0A may need to be changed to UCSRA or UCSRnA where n is the number of the uart you want to use.
+ * I made not effort to make this general, since it is an easy switch to another uart if you wish.
+ * 
+ * Some control of the code is done with #define statements in this header to keep the code small. It
+ * uses less than 1.5k, quite a bit less than Arduino HardwareSerial, though that code has different functionality.
+ * To be fair, if you add in the printf functionality, code size goes to ~ 3k.
+ *
+ * For really small code, turn off the buffering with the UART_USE_BUFFERS flag, but loose the buffers and 
+ * interrupt routines
+ *
+ * If you keep the buffers, then bit0 if the mode flags disables (0) or enables (1) the transmit (TX) buffering.
+ * and bit1 does so for the receive (RX). It can be useful when debugging code to turn off the buffers temporarily.
+ *
+ * Author: Maurik Holtrop
+ * Date:   2016-12-26
+ *
+ * Yes, this code is 'pedagogically' commented. Deal with it.
+**/
+
+#ifndef __UART_CPP__
+#define __UART_CPP__
+
+#include <stdio.h>
+
+#ifndef F_CPU
+#error This code will not work without properly defining F_CPU
+#endif
+
+#ifndef BAUD
+#error This code will not work properly without defining BAUD
+#endif
+
+//
+// Define varipus behaviors of the UART code.
+//
+
+#ifndef UART_ALLOW_BAUD
+#define UART_ALLOW_BAUD 1          // Set to 0 fixes BAUD rate and saves space (320 bytes)
+#define BAUD_TOL 2                 // Otherwise set in util/setbaud.h
+#endif
+
+#ifndef UART_ECHO
+#define UART_ECHO 0                // Set to 1 if you want each keystroke echoed back immediately.
+#endif
+
+#ifndef UART_BLOCK_ON_OVERFLOW
+#define UART_BLOCK_ON_OVERFLOW 0   // Set to one if you want to block on overflow, so no chars go missing.
+// Note that this is problematic if you depend on the interrupt routines to not take much time.
+// If you set this to 0 then on overflow an @ is printed and characters are discarded from the buffer.
+#endif
+
+#ifndef UART_USE_BUFFERS
+#define UART_USE_BUFFERS  0         // Set to 1 if you want buffered code; 0 no buffers, so this all gets smaller.
+#endif
+
+#if UART_USE_BUFFERS == 1
+//
+// Buffer sizes. Note that these go into the RAM, which is very limited on most chips.
+//
+#ifndef TX_UART_BUF_SIZE
+#define TX_UART_BUF_SIZE 64
+#endif
+#ifndef RX_UART_BUF_SIZE
+#define RX_UART_BUF_SIZE 64
+#endif
+
+#if (TX_UART_BUF_SIZE>256)
+typedef uint16_t __tx_uart_buf_index_t;
+#else
+typedef uint8_t __tx_uart_buf_index_t;
+#endif
+
+#if (RX_UART_BUF_SIZE>256)
+typedef uint16_t __rx_uart_buf_index_t;
+#else
+typedef uint8_t __rx_uart_buf_index_t;
+#endif
+
+extern bool __rx_uart_receive_complete;
+///
+extern __tx_uart_buf_index_t __tx_uart_buf_head;
+extern __tx_uart_buf_index_t __tx_uart_buf_tail;
+extern __tx_uart_buf_index_t __rx_uart_buf_head;
+extern __tx_uart_buf_index_t __rx_uart_buf_tail;
+
+inline bool uart_receive_complete(void){
+  return (__rx_uart_receive_complete);
+}
+
+#else
+
+inline bool uart_receive_complete(void){
+  return(true);
+}
+
+#endif  // UART_USE_BUFFERS
+///
+
+void uart_tx_buffer_flush();       //! Flush the buffers. This is a blocking operation for transmit.
+void uart_init(unsigned char mode=0,uint32_t baud=BAUD); //! Initialize the UART code.
+bool uart_receive_complete(void);  //! Return the receive complete flag. Indicates a return (RET) or newline '\n' on the RX.
+
+int  uart_putchar_buffered(char c, FILE *stream); //! Internal routine, buffered put.
+int  uart_putchar(char c, FILE *stream);          //! Internal routine, unbuffered put.
+int  uart_getchar_buffered(FILE *stream);         //! Internal routing, buffered get.
+int  uart_getchar(FILE *stream);                  //! Internal routine, unbuffered get.
+
+
+#endif
