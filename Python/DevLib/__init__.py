6663873da46d35d085c9bd94d10ab8b4499ec20e
Maurik Holtrop <maurik@physics.unh.edu>
2018-05-31 11:28:32 -0400

    Consolidate BBB and RPi versions of the DevLib

---
+++ Python/DevLib/__init__.py
@@ -0,0 +1,16 @@
+#
+# Here we initialize the modules.
+#
+
+__all__ = ["AD9850","BME280","DS3231","ISL29125","MAX7219","MCP320x","MCP4251","SI7021","SN74HC165","SSD1331"]
+
+from .AD9850     import AD9850
+from .BME280     import BME280
+from .DS3231     import DS3231
+from .ISL29125   import ISL29125
+from .MAX7219    import MAX7219
+from .MCP320x    import MCP320x
+from .MCP4251    import MCP4251
+from .SI7021     import SI7021
+from .SN74HC165  import SN74HC165
+from .SSD1331    import SSD1331
