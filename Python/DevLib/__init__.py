d175d5a6330de7b6b1a2b54fed1f2c37550adab6
Maurik Holtrop <maurik@physics.unh.edu>
2019-04-22 20:17:01 -0400

    update init

--- Python/DevLib/__init__.py
+++ Python/DevLib/__init__.py
@@ -6,6 +6,7 @@ __all__ = ["AD9850","APA102","BBSpiDev","BME280","DS3231","ISL29125","MAX7219","
 
 from .AD9850     import AD9850
 from .APA102     import APA102
+from .ADS1115    import ADS1115
 from .BBSpiDev   import BBSpiDev
 from .BME280     import BME280
 from .DS3231     import DS3231
@@ -14,6 +15,7 @@ from .ISL29125   import ISL29125
 from .MAX7219    import MAX7219
 from .MCP320x    import MCP320x
 from .MCP4251    import MCP4251
+from .MCP4725    import MCP4725
 from .SI7021     import SI7021
 from .SN74HC165  import SN74HC165
 from .SSD1331    import SSD1331
