7578725968a492c6c2375812e80d1ad34ea67da7
Maurik Holtrop <maurik@physics.unh.edu>
2023-04-14 08:49:33 -0400

    The extra bits in the control word should be ignored, but better to set them to zero. Additional cleanliness edits.

--- Python/DevLib/MCP320x.py
+++ Python/DevLib/MCP320x.py
@@ -50,80 +50,80 @@ except:
 
 import spidev
 
+
 class MCP320x:
 
     class my_values:
         """Class for getting the value of the chip, which mimics a list."""
-        def __init__(self,getter,MAX):
+        def __init__(self, getter, MAX):
             self._getter = getter
             self._MAX = MAX
             self._n = 0
 
-        def __getitem__(self,idx):
-            return(self._getter(idx))
+        def __getitem__(self, idx):
+            return self._getter(idx)
 
-        def __setitem__(self,idx,val):
+        def __setitem__(self, idx, val):
             raise ValueError("The ADC values cannot be written to, only read.")
 
         def __len__(self):
-            return(self._MAX)
+            return self._MAX
 
         def __iter__(self):
             self._n=0
-            return(self)
+            return self
 
         def __next__(self):
             if self._n < len(self):
                 result = self[self._n]
                 self._n += 1
-                return(result)
+                return result
             else:
                 raise StopIteration
 
         def __repr__(self):
-            return(str(self))
+            return str(self)
 
         def __str__(self):
             tmplist = [ x for x in self ]
-            return(str(tmplist))
+            return str(tmplist)
 
-
-    def __init__(self,CS_bar_pin,CLK_pin=1000000,MOSI_pin=0,MISO_pin=0,chip='MCP3208',
-                 channel_max=None,bit_length=None,single_ended=True):
-        '''Initialize the code and set the GPIO pins.
+    def __init__(self, CS_bar_pin, CLK_pin=1000000, MOSI_pin=0, MISO_pin=0,chip='MCP3208',
+                 channel_max=None, bit_length=None, single_ended=True):
+        """Initialize the code and set the GPIO pins.
         The last argument, ch_max, is 2 for the MCP3202, 4 for the
-        MCP3204 or 8 for the MCS3208'''
+        MCP3204 or 8 for the MCS3208"""
 
         self._CLK = CLK_pin
         self._MOSI = MOSI_pin
         self._MISO = MISO_pin
         self._CS_bar = CS_bar_pin
 
-        chip_dictionary={
-                "MCP3202":(2,12),
-                "MCP3204":(4,12),
-                "MCP3208":(8,12),
-                "MCP3002":(2,10),
-                "MCP3004":(4,10),
-                "MCP3008":(8,10)
+        chip_dictionary = {
+                "MCP3202": (2, 12),
+                "MCP3204": (4, 12),
+                "MCP3208": (8, 12),
+                "MCP3002": (2, 10),
+                "MCP3004": (4, 10),
+                "MCP3008": (8, 10)
         }
 
         if chip in chip_dictionary:
             self._ChannelMax = chip_dictionary[chip][0]
-            self._BitLength  = chip_dictionary[chip][1]
-        elif chip == None and (channel_max is not None) and (bit_length is not None):
+            self._BitLength = chip_dictionary[chip][1]
+        elif chip is None and (channel_max is not None) and (bit_length is not None):
             self._ChannelMax = channel_max
-            self._BitLength  = bit_length
+            self._BitLength = bit_length
         else:
             print("Unknown chip: {} - Please re-initialize.")
             self._ChannelMax = 0
-            self._BitLength  = 0
+            self._BitLength = 0
             return
 
-        self._SingleEnded = single_ended;
+        self._SingleEnded = single_ended
         self._Vref = 3.3
-        self._values = self.my_values(self.ReadADC,self._ChannelMax)
-        self._volts  = self.my_values(self.ReadVolts,self._ChannelMax)
+        self._values = self.my_values(self.ReadADC, self._ChannelMax)
+        self._volts  = self.my_values(self.ReadVolts, self._ChannelMax)
 
         # This is used to speed up the SPIDEV communication. Send out MSB first.
         # control[0] - bit7-3: upper 5 bits 0, because we can only send 8 bit sequences.
@@ -134,63 +134,63 @@ class MCP320x:
         #            - bit6   : D0 low bit of channel select.
         #            - bit5-0 : Don't care.
         if self._SingleEnded:
-            self._control0=[0b00000110,0b00100000,0]  # Pre-compute part of the control word.
+            self._control0 = [0b00000110, 0b00000000, 0]  # Pre-compute part of the control word.
         else:
-            self._control0=[0b00000100,0b00100000,0]  # Pre-compute part of the control word.
+            self._control0 = [0b00000100, 0b00000000, 0]  # Pre-compute part of the control word.
 
-        if self._MOSI > 0:  # Bing Bang mode
+        if self._MOSI > 0:  # Bit Bang mode
             assert self._MISO !=0 and self._CLK < 32
             if GPIO.getmode() != 11:
                 GPIO.setmode(GPIO.BCM)        # Use the BCM numbering scheme
 
-            GPIO.setup(self._CLK,GPIO.OUT)     # Setup the ports for in and output
-            GPIO.setup(self._MOSI,GPIO.OUT)
-            GPIO.setup(self._MISO,GPIO.IN)
-            GPIO.setup(self._CS_bar,GPIO.OUT)
+            GPIO.setup(self._CLK, GPIO.OUT)     # Setup the ports for in and output
+            GPIO.setup(self._MOSI, GPIO.OUT)
+            GPIO.setup(self._MISO, GPIO.IN)
+            GPIO.setup(self._CS_bar, GPIO.OUT)
 
-            GPIO.output(self._CLK,0)           # Set the clock low.
-            GPIO.output(self._MOSI,0)          # Set the Master Out low
-            GPIO.output(self._CS_bar,1)        # Set the CS_bar high
+            GPIO.output(self._CLK, 0)           # Set the clock low.
+            GPIO.output(self._MOSI, 0)          # Set the Master Out low
+            GPIO.output(self._CS_bar, 1)        # Set the CS_bar high
 
         else:
-            self._dev = spidev.SpiDev(0,self._CS_bar) # Start a SpiDev device
+            self._dev = spidev.SpiDev(0, self._CS_bar)  # Start a SpiDev device
             self._dev.mode = 0                    # Set SPI mode (phase)
             self._dev.max_speed_hz = self._CLK     # Set the data rate
             self._dev.bits_per_word = 8           # Number of bit per word. ALWAYS 8
 
     def __del__(self):
-        ''' Cleanup the GPIO before being destroyed '''
-        if(self._MOSI>0):
+        """ Cleanup the GPIO before being destroyed """
+        if self._MOSI > 0:
             GPIO.cleanup(self._CS_bar)
             GPIO.cleanup(self._CLK)
             GPIO.cleanup(self._MOSI)
             GPIO.cleanup(self._MISO)
 
     def get_channel_max(self):
-        '''Return the maximum number of channels'''
-        return(self._ChannelMax)
+        """Return the maximum number of channels"""
+        return self._ChannelMax
 
     def get_bit_length(self):
-        '''Return the number of bits that will be read'''
-        return(self._BitLength)
+        """Return the number of bits that will be read"""
+        return self._BitLength
 
     def get_value_max(self):
-        '''Return the maximum value possible for an ADC read'''
-        return(2**self._BitLength-1)
+        """Return the maximum value possible for an ADC read."""
+        return 2**self._BitLength-1
 
-    def SendBit(self,bit):
-        ''' Send out a single bit, and pulse clock.'''
+    def SendBit(self, bit):
+        """Send out a single bit, and pulse clock."""
         if self._MOSI == 0:
             return
         #
         # The input is read on the rising edge of the clock.
         #
-        GPIO.output(self._MOSI,bit) # Set the bit.
-        GPIO.output(self._CLK,1)    # Rising edge sends data
-        GPIO.output(self._CLK,0)    # Return clock to zero.
+        GPIO.output(self._MOSI, bit)  # Set the bit.
+        GPIO.output(self._CLK, 1)    # Rising edge sends data
+        GPIO.output(self._CLK, 0)    # Return clock to zero.
 
     def ReadBit(self):
-        ''' Read a single bit from the ADC and pulse clock.'''
+        """Read a single bit from the ADC and pulse clock."""
         if self._MOSI == 0:
             return 0
         #
@@ -198,31 +198,30 @@ class MCP320x:
         # and is to be read on the rising edge of the clock.
 
         # Clock should be already low, and data should already be set.
-        GPIO.output(self._CLK,1) # Set the clock high. Ready to read.
-        bit = GPIO.input(self._MISO) # Read the bit.
-        GPIO.output(self._CLK,0) # Return clock low, next bit will be set.
+        GPIO.output(self._CLK, 1)  # Set the clock high. Ready to read.
+        bit = GPIO.input(self._MISO)  # Read the bit.
+        GPIO.output(self._CLK, 0)  # Return clock low, next bit will be set.
 
-        return(bit)
+        return bit
 
-
-    def ReadADC(self,channel):
-        '''This reads the actual ADC value, after connecting the analog multiplexer to
+    def ReadADC(self, channel):
+        """This reads the actual ADC value, after connecting the analog multiplexer to
         the desired channel.
         ADC value is returned at a n-bit integer value, with n=10 or 12 depending on the chip.
         The value can be converted to a voltage with:
-           volts = data*Vref/(2**n-1)'''
+           volts = data*Vref/(2**n-1)"""
         if channel < 0 or channel >= self._ChannelMax:
             print("Error - chip does not have channel = {}".format(channel))
-
+            return 0
 
         if self._MOSI == 0:
             # SPIdev Code
             # This builds up the control word, which selects the channel
             # and sets single/differential more.
-            control=[ self._control0[0] + ((channel&0b100)>>2) , self._control0[1]+((channel&0b011)<<6),0]
+            control = [self._control0[0] + ((channel&0b100) >> 2), self._control0[1]+((channel & 0b011) <<6), 0]
             dat = self._dev.xfer(control)
-            value= (dat[1]<<8)+dat[2] # Unpack the two 8-bit words to a single integer.
-            return(value)
+            value = (dat[1] << 8)+dat[2]  # Unpack the two 8-bit words to a single integer.
+            return value
 
         else:
             # Bit Bang code.
@@ -234,99 +233,98 @@ class MCP320x:
             #
             # Start of sequence sets CS_bar low, and sends sequence
             #
-            GPIO.output(self._CLK,0)                # Make sure clock starts low.
-            GPIO.output(self._MOSI,0)
-            GPIO.output(self._CS_bar,0)             # Select the chip.
+            GPIO.output(self._CLK, 0)                # Make sure clock starts low.
+            GPIO.output(self._MOSI, 0)
+            GPIO.output(self._CS_bar, 0)             # Select the chip.
             self.SendBit(1)                        # Start bit = 1
             self.SendBit(self._SingleEnded)   # Select single or differential
             if self._ChannelMax > 2:
-                self.SendBit(int( (channel & 0b100)>0) ) # Send high bit of channel = DS2
-                self.SendBit(int( (channel & 0b010)>0) ) # Send mid  bit of channel = DS1
-                self.SendBit(int( (channel & 0b001)>0) ) # Send low  bit of channel = DS0
+                self.SendBit(int((channel & 0b100) > 0))  # Send high bit of channel = DS2
+                self.SendBit(int((channel & 0b010) > 0))  # Send mid  bit of channel = DS1
+                self.SendBit(int((channel & 0b001) > 0))  # Send low  bit of channel = DS0
             else:
                 self.SendBit(channel)
             self.SendBit(1)                       # MSB First (for MCP3x02) or don't care.
 
             # The clock is currently low, and the dummy bit = 0 is on the ouput of the ADC
             #
-            dummy = self.ReadBit() # Read the bit.
+            dummy = self.ReadBit()  # Read the bit.
             #if dummy != 0:
             #    print "We expected a 0, dummy bit but we got a 1. Something is wrong here."
             data = 0
             for i in range(self._BitLength):
-                data <<= 1                     # Note you need to shift left first, or else you shift the last bit (bit 0) to the 1 position.
+                data <<= 1
+                # Note you need to shift left first, or else you shift the last bit (bit 0) to the 1 position.
                 bit = self.ReadBit()
                 data += bit
 
-            GPIO.output(self._CS_bar,1)  # Unselect the chip.
+            GPIO.output(self._CS_bar, 1)  # Unselect the chip.
 
-            return(data)
+            return data
 
-    def ReadVolts(self,channel):
+    def ReadVolts(self, channel):
         """Read the ADC value from channel and convert to volts, assuming that Vref is set correctly. """
-        return( self._Vref*self.ReadADC(channel)/self.get_value_max())
+        return self._Vref*self.ReadADC(channel)/self.get_value_max()
 
     def FastReadADC0(self):
-        '''This reads the actual ADC value of channel 0, with as little overhead as possible.
+        """his reads the actual ADC value of channel 0, with as little overhead as possible.
         Use with SPIDEV ONLY!!!!
-        returns: The ADC value as an n-bit integer value, with n=10 or 12 depending on the chip.'''
+        returns: The ADC value as an n-bit integer value, with n=10 or 12 depending on the chip."""
 
         dat = self._dev.xfer(self._control0)
-        value= (dat[1]<<8)+dat[2]
-        return(value)
+        value = (dat[1] << 8)+dat[2]
+        return value
 
     @property
     def values(self):
         """ADC values presented as a list."""
-        return( self._values )
+        return self._values
 
     @property
     def volts(self):
         """ADC voltages presented as a list"""
-        return( self._volts )
+        return self._volts
 
     @property
     def accuracy(self):
         """The fractional voltage of the least significant bit. """
-        return(self._Vref/float(self.get_value_max()))
+        return self._Vref/float(self.get_value_max())
 
     @property
     def Vref(self):
         """Reference voltage used by the chip. You need to set this. It defaults to 3.3V"""
-        return(self._Vref)
+        return self._Vref
 
     @Vref.setter
-    def Vref(self,vr):
+    def Vref(self, vr):
         self._Vref = vr
 
 
-
-
 def main(argv):
-    '''Test code for the MCP320x driver. This assumes you are using a MCP3208
-    If no arguments are supplied, then use SPIdev for CE0 and read channel 0'''
+    """Test code for the MCP320x driver. This assumes you are using a MCP3208
+    If no arguments are supplied, then use SPIdev for CE0 and read channel 0"""
 
     if len(argv) < 2:
-        cs_bar=0
-        clk_pin=1000000
-        mosi_pin=0
-        miso_pin=0
-        channel =0
+        cs_bar = 0
+        clk_pin = 1000000
+        mosi_pin = 0
+        miso_pin = 0
+        channel = 0
     elif len(argv) < 6:
         print("Please supply: cs_bar_pin clk_pin mosi_pin miso_pin channel")
         sys.exit(1)
 
     else:
-        cs_bar  = int(argv[1])
+        cs_bar = int(argv[1])
         clk_pin = int(argv[2])
-        mosi_pin= int(argv[3])
-        miso_pin= int(argv[4])
+        mosi_pin = int(argv[3])
+        miso_pin = int(argv[4])
         channel = int(argv[5])
 
-    A = MCP320x(cs_bar,clk_pin,mosi_pin,miso_pin)
+    A = MCP320x(cs_bar, clk_pin, mosi_pin, miso_pin)
     try:
         while True:
-            value=A.ReadADC(channel)
+            value = A.ReadADC(channel)
             print("{:4d}".format(value))
             time.sleep(0.1)
     except KeyboardInterrupt:
@@ -336,5 +334,4 @@ def main(argv):
 if __name__ == '__main__':
     import sys
     import time
-
     main(sys.argv)
