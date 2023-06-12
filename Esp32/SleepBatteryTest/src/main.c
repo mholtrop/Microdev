6959df2efe91114752381a9f672ee9197f9d7cd9
Maurik Holtrop <maurik@physics.unh.edu>
2023-06-12 08:53:36 -0400

    Working version.

--- Esp32/SleepBatteryTest/src/main.c
+++ Esp32/SleepBatteryTest/src/main.c
@@ -15,6 +15,7 @@
 #include "esp_adc/adc_oneshot.h"
 #include "esp_adc/adc_cali.h"
 #include "esp_adc/adc_cali_scheme.h"
+#include "esp_phy_init.h"
 #include "esp_sntp.h"                                                                                                                               
 #include "esp_sleep.h"
 
@@ -25,17 +26,34 @@
 #define QUOTE(x) Q(x)
 
 #ifndef CHIP_NUMBER_NUM 
+#define CHIP_NUMBER_NUM 1
 #define CHIP_NUMBER QUOTE(1)
 #else
 #define CHIP_NUMBER QUOTE(CHIP_NUMBER_NUM)
 #endif
 
+#if CHIP_NUMBER_NUM < 3
 #define USER_INFO_PIN GPIO_NUM_9
-#define USER_LED GPIO_NUM_21 // GPIO_NUM_21
+#define USER_LED      GPIO_NUM_21
+#define BATTERY_MONITOR ADC_CHANNEL_0
+#define BATTERY_GPIO_NUM GPIO_NUM_1 
+#else
+#define USER_INFO_PIN GPIO_NUM_9
+#define USER_LED      GPIO_NUM_8 
+#define BATTERY_MONITOR ADC_CHANNEL_2
+#define BATTERY_GPIO_NUM GPIO_NUM_2
+#endif
+
+#define ADC1_ATTEN           ADC_ATTEN_DB_11	//ADC_ATTEN_DB_0 = No input attenuation, ADC can measure up to Vref which is approx 1100mV (factory test value burnt into eFuse)
+												//ADC_ATTEN_DB_2_5 = input attenuated extending the range of measurement by about 2.5 dB (1.33 x)
+												//ADC_ATTEN_DB_6 = input attenuated extending the range of measurement by about 6 dB (2 x)
+												//ADC_ATTEN_DB_11 = input attenuated extending the range of measurement by about 11 dB (3.55 x)
+
+
 #define USER_LED_ON 0
 #define USER_LED_OFF 1
 
-#define ESP_MAXIMUM_RETRY 10
+#define ESP_MAXIMUM_RETRY 3
 #define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_PSK
 #define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HUNT_AND_PECK
 #define EXAMPLE_H2E_IDENTIFIER ""
@@ -46,21 +64,35 @@ static RTC_DATA_ATTR unsigned int boot_count = 0;
 /* FreeRTOS event group to signal when we are connected*/
 static EventGroupHandle_t s_wifi_event_group;
 
-/* FreeRTOS event queue for inter process communication.*/
-QueueHandle_t gpio_evt_queue = NULL;
-QueueHandle_t gpio_phase_queue = NULL;
-
 /* The event group allows multiple bits for each event, but we only care about two events:
  * - we are connected to the AP with an IP
  * - we failed to connect after the maximum amount of retries */
-#define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
-#define MQTT_CONNECTED_BIT BIT2
-#define MQTT_FAIL_BIT      BIT3
-#define MQTT_PUBLISHED_BIT BIT4
+#define WIFI_CONNECTED_BIT    BIT0
+#define WIFI_HAS_AP_BIT       BIT1
+#define WIFI_DISCONNECTED_BIT BIT2
+#define WIFI_STOP_BIT         BIT3
+#define WIFI_FAIL_BIT         BIT4
+
+#define MQTT_CONNECTED_BIT BIT6
+#define MQTT_FAIL_BIT      BIT7
+#define MQTT_PUBLISHED_BIT BIT8
+
+#define WIFI_STATUS_START 0
+#define WIFI_STATUS_TRY_CONNECT 1
+#define WIFI_STATUS_CONNECTED 2
+#define WIFI_STATUS_DISCONNECTED 3
+#define WIFI_STATUS_STOP 4
 
 /* Count the number of connection tries.*/
+static int s_wifi_status = -1;
 static int s_retry_num = 0;
+
+/* Specify the deep sleep time */
+static unsigned long s_deep_sleep_time = 17000000ULL;
+
+/* FreeRTOS event queue for inter process communication.*/
+QueueHandle_t gpio_evt_queue = NULL;
+QueueHandle_t gpio_phase_queue = NULL;
 
 
 
@@ -101,30 +133,42 @@ static void handle_wifi_connection(void *arg, esp_event_base_t event_base, int32
             case WIFI_EVENT_STA_START:
                 printf("Wfi Event Start, try esp_wifi_connect()\n");
                 fflush(stdout);
+                s_wifi_status = WIFI_STATUS_TRY_CONNECT;
                 esp_wifi_connect();
                 break;
             case WIFI_EVENT_STA_DISCONNECTED:
-                if (s_retry_num < ESP_MAXIMUM_RETRY) {
-                    esp_wifi_connect();
-                    s_retry_num++;
-                    printf("retry to connect to the AP\n");
-                    fflush(stdout);
+                if (s_wifi_status == WIFI_STATUS_TRY_CONNECT){
+                    if (s_retry_num < ESP_MAXIMUM_RETRY) {
+                        printf("retry to connect to the AP\n");
+                        fflush(stdout);
+                        esp_wifi_connect();
+                        s_retry_num++;
+                   } else {
+                        printf("!!!!! connect to the AP failed !!!!!!\n");
+                        fflush(stdout);
+                        xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+                    }
                 } else {
-                    printf("!!!!! connect to the AP failed !!!!!!\n");
+                    printf("Wifi disconnecting\n");
                     fflush(stdout);
-                    xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+                    xEventGroupSetBits(s_wifi_event_group, WIFI_DISCONNECTED_BIT);
+                    s_wifi_status = WIFI_STATUS_DISCONNECTED;
                 }
             break;
             case WIFI_EVENT_STA_CONNECTED:
                 printf("connected to AP\n");
                 fflush(stdout);
+                s_wifi_status = WIFI_STATUS_CONNECTED;
+                xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
             break;
             case WIFI_EVENT_STA_STOP:
-                printf("Wifi stopped \n");
+                printf("Wifi stopping \n");
                 fflush(stdout);
+                s_wifi_status = WIFI_STATUS_STOP;
+                xEventGroupSetBits(s_wifi_event_group, WIFI_STOP_BIT);
             break;
             default:
-                printf("Wifi event %d\n", event_id);
+                printf("Wifi event %ld\n", event_id);
                 fflush(stdout);
         }
     } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
@@ -132,7 +176,7 @@ static void handle_wifi_connection(void *arg, esp_event_base_t event_base, int32
         printf("got ip: " IPSTR " \n", IP2STR(&event->ip_info.ip));
         fflush(stdout);
         s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+        xEventGroupSetBits(s_wifi_event_group, WIFI_HAS_AP_BIT);
     } else {
         printf("UNEXPECTED EVENT: %s  id: %ld\n", event_base, event_id);
         fflush(stdout);
@@ -140,12 +184,13 @@ static void handle_wifi_connection(void *arg, esp_event_base_t event_base, int32
 }
 
 void wifi_start(void){
-       // Start the event loop, and register WiFi callbacks.
+    // Start the event loop, and register WiFi callbacks.
     // Can replace the last NULL with a pointer to an esp_event_handler_instance_t.
     esp_event_loop_create_default();
     esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &handle_wifi_connection, NULL, NULL);
     esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &handle_wifi_connection, NULL, NULL);
     
+    s_wifi_status = WIFI_STATUS_START;
 
    wifi_config_t wifi_config = {
     .sta = {
@@ -224,11 +269,11 @@ static void handle_mqtt_events(void *handler_args,
     fflush(stdout);
 }
 
-void time_sync_notification_cb(struct timeval *tv)
-{
-    ESP_LOGI(TAG, "Notification of a time synchronization event");
-}
-
+// void time_sync_notification_cb(struct timeval *tv)
+// {
+//     ESP_LOGI(TAG, "Notification of a time synchronization event");
+// }
+//
 // static void initialize_sntp(void)
 // {
 //     ESP_LOGI(TAG, "Initializing SNTP");
@@ -247,7 +292,7 @@ void app_main() {
     gpio_config_t io_conf = {
         .intr_type = GPIO_INTR_DISABLE,
         .mode = GPIO_MODE_INPUT,
-        .pin_bit_mask = (1ULL << GPIO_NUM_1),
+        .pin_bit_mask = (1ULL << BATTERY_GPIO_NUM),
         .pull_down_en = 0,
         .pull_up_en = 0
     };
@@ -266,6 +311,7 @@ void app_main() {
     vTaskDelay(5000 / portTICK_PERIOD_MS);
     boot_count++;
     printf("Sleep Battery Test " CHIP_NUMBER " start. Boot # %d\n",boot_count);
+    printf(" ADC Channel used = %d  %d\n", BATTERY_MONITOR, BATTERY_GPIO_NUM);
     fflush(stdout);
     
    // The NVS partition is used by the WiFi library.
@@ -292,24 +338,41 @@ void app_main() {
         printf("connected to Wifi\n");
         gpio_set_level(USER_LED, USER_LED_OFF); 
     } else if (bits & WIFI_FAIL_BIT) {
-        printf("Failed to connect to Wifi\n");
-        for(int i=0; i<20; i++){
+        printf("Failed to connect to Wifi. Erase PHY Calibration, then doing full reboot. \n");
+        fflush(stdout);
+        esp_phy_erase_cal_data_in_nvs();
+        for(int i=0; i<10; i++){
             gpio_set_level(USER_LED, USER_LED_ON); 
             vTaskDelay(200 / portTICK_PERIOD_MS);
             gpio_set_level(USER_LED, USER_LED_OFF); 
             vTaskDelay(200 / portTICK_PERIOD_MS);
         }
-        esp_wifi_disconnect();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        esp_wifi_stop();
-        esp_deep_sleep(1000ULL);
-        // esp_restart();  // Hard restart will reset boot count.
+        // esp_wifi_disconnect();
+        // bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_DISCONNECTED_BIT, pdTRUE, pdFALSE, 10000/portTICK_PERIOD_MS);
+        // printf("Disconnected from Wifi\n");
+        // fflush(stdout);
+        // esp_wifi_stop();
+        // printf("Wifi stopped\n");
+        // fflush(stdout);
+        // esp_wifi_deinit();
+        // esp_deep_sleep(1000ULL);
+        esp_restart();  // Hard restart will reset boot count, but also fully reset the WiFi. Chip #2 does allow WiFi after deep sleep :-(.
     } else {
         printf("UNEXPECTED EVENT\n");
     }
     gpio_set_level(USER_INFO_PIN, 0);
     printf("Boot num: %d\n",boot_count);
     fflush(stdout);
+
+    bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_HAS_AP_BIT, pdTRUE, pdFALSE, portMAX_DELAY);
+
+    if (bits & WIFI_HAS_AP_BIT) {
+        printf("AP started\n");
+        fflush(stdout);
+    } else {
+        printf("AP failed to start\n");
+        fflush(stdout);
+    }
 
     /* Now we can start up the MQTT system. */
     esp_mqtt_client_config_t mqtt_cfg = {
@@ -321,35 +384,8 @@ void app_main() {
     esp_log_level_set("mqtt_client",ESP_LOG_INFO);     // Silence the MQTT client debug, to avoind the timeout messages.
     esp_log_level_set("transport_base",ESP_LOG_ERROR); // Same with the transport_base.
 
-    // // Setup Time. See: https://esp32tutorials.com/esp32-sntp-esp-idf-synchronize-time-ntp/
-    // initialize_sntp();
-    // time_t now = 0;
-    // struct tm timeinfo = { 0 };
-    // int retry = 0;
-    // const int retry_count = 10;
-    // while (sntp_get_sync_status() == SNTP_SYNC_STATUS_RESET && ++retry < retry_count) {
-    //     ESP_LOGI(TAG, "Waiting for system time to be set... (%d/%d)", retry, retry_count);
-    //     vTaskDelay(2000 / portTICK_PERIOD_MS);
-    // }
-    // time(&now);
-    // localtime_r(&now, &timeinfo);
-    // char strftime_buf[64];
-
-    // // Set timezone to Eastern Standard Time and print local time
-    // setenv("TZ", "EST5EDT,M3.2.0/2,M11.1.0", 1);
-    // tzset();
-    // localtime_r(&now, &timeinfo);
-    // strftime(strftime_buf, sizeof(strftime_buf), "%c", &timeinfo);
-    // ESP_LOGI(TAG, "The current date/time in New York is: %s", strftime_buf);
-
-
     // Setup to read the battery voltage from A0. 
     // This should have two resistors of ~470k in a divider.
-
-    #define ADC1_ATTEN           ADC_ATTEN_DB_11	//ADC_ATTEN_DB_0 = No input attenuation, ADC can measure up to Vref which is approx 1100mV (factory test value burnt into eFuse)
-												//ADC_ATTEN_DB_2_5 = input attenuated extending the range of measurement by about 2.5 dB (1.33 x)
-												//ADC_ATTEN_DB_6 = input attenuated extending the range of measurement by about 6 dB (2 x)
-												//ADC_ATTEN_DB_11 = input attenuated extending the range of measurement by about 11 dB (3.55 x)
 
     adc_oneshot_unit_handle_t Adc1Handle;
     adc_cali_handle_t Adc1CalibrationHandle = NULL;
@@ -366,7 +402,7 @@ void app_main() {
 		.bitwidth = ADC_BITWIDTH_DEFAULT,
 		.atten = ADC1_ATTEN,
 	};
-	ESP_ERROR_CHECK(adc_oneshot_config_channel(Adc1Handle, ADC_CHANNEL_0, &Adc1Config));
+	ESP_ERROR_CHECK(adc_oneshot_config_channel(Adc1Handle, BATTERY_MONITOR, &Adc1Config));
 
 	//Calibration Init
 	ADCCalInitialise(ADC_UNIT_1, ADC1_ATTEN, &Adc1CalibrationHandle);
@@ -394,7 +430,7 @@ void app_main() {
         gpio_set_level(USER_INFO_PIN, 1);
         
         //----- READ ADC_CHANNEL_0 -----
-        if (adc_oneshot_read(Adc1Handle, ADC_CHANNEL_0, &AdcRawValueChannel_0) == ESP_OK)		//<<Takes approx 130uS on ESP32S3
+        if (adc_oneshot_read(Adc1Handle, BATTERY_MONITOR, &AdcRawValueChannel_0) == ESP_OK)		//<<Takes approx 130uS on ESP32S3
         {
             printf("Boot %d -- ADC Raw: %d", boot_count, AdcRawValueChannel_0);
             adc_cali_raw_to_voltage(Adc1CalibrationHandle, AdcRawValueChannel_0, &AdcVoltageChannel_0);
@@ -444,9 +480,14 @@ void app_main() {
     gpio_set_level(USER_LED, USER_LED_ON);
     esp_mqtt_client_stop(mqtt_client);
     esp_wifi_disconnect();
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    bits = xEventGroupWaitBits(s_wifi_event_group, WIFI_DISCONNECTED_BIT, pdTRUE, pdFALSE, 10000/portTICK_PERIOD_MS);
+    printf("Disconnected from Wifi\n");
+    fflush(stdout);
     esp_wifi_stop();
+    esp_wifi_deinit();
+    printf("Wifi stopped\n");
+    fflush(stdout);
     printf("Going to deep sleep now\n");
     gpio_set_level(USER_LED, USER_LED_OFF);  // User LED off.
-    esp_deep_sleep(57000000ULL); /* One minute - 3s of deep sleep, then it reboots. */
+    esp_deep_sleep(s_deep_sleep_time); /* ~One minute - 3s of deep sleep, then it reboots. */
 }
